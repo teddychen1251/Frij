9fcfa68422aa0e698b4bac8ac566526bab4cd814
justinjhu <justinjiaxinghu@gmail.com>
2019-10-26 05:24:55 -0400

    added org

--- src/RegisterScreen.js
+++ src/RegisterScreen.js
@@ -4,22 +4,28 @@ import { TextInput, Image, Text, View } from 'react-native';
 class RegisterScreen extends React.Component {
     render() {
         return (
-          <View style={{justifyContent: 'center'}}>
+          <View style={{flexDirection: 'column', justifyContent: 'center'}}>
             <View style = {{justifyContent: 'center', alignItems: 'center'}}>
               <Image source = {require('./assets/frijlogo.png')} style={{width: '50%', height: '50%'}} resizeMode = "contain"/>
             </View>
-            <TextInput
-              style={{marginLeft : '38%', marginRight : '33%', height: 40}}
-              placeholder = "Create username"
-            />
-            <TextInput
-              style={{marginLeft : '38%', marginRight : '33%', height: 40}}
-              placeholder = "Create password"
-            />
-            <TextInput
-              style={{marginLeft : '38%', marginRight : '33%', height: 40}}
-              placeholder = "Verify password"
-            />
+            <View style = {{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+              <TextInput
+                style={{marginLeft : '38%', marginRight : '33%', height: 40}}
+                placeholder = "Organization:"
+              />
+              <TextInput
+                style={{marginLeft : '38%', marginRight : '33%', height: 40}}
+                placeholder = "Create username:"
+              />
+              <TextInput
+                style={{marginLeft : '38%', marginRight : '33%', height: 40}}
+                placeholder = "Create password:"
+              />
+              <TextInput
+                style={{marginLeft : '38%', marginRight : '33%', height: 40}}
+                placeholder = "Verify password:"
+              />
+            </View>
           </View>
         )
     }
